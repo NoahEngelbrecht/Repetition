bc8b0f939916de165843defbba12fabc6b53e704
NoahEngelbrecht <noah.engelbrecht@elev.ga.ntig.se>
2025-03-04 10:06:37 +0100

    Add files via upload

---
+++ Repetition.py
@@ -0,0 +1,66 @@
+# def kontrollera_tal(tal):
+#     if tal % 2 == 0:
+#         print("Talet är jämnt")
+#     else:
+#         print("Talet är udda")
+
+# kontrollera_tal(2)
+
+# variabel = "python är roligt!"
+
+# print(variabel.upper())
+# print(variabel.lower())
+# print(variabel.capitalize())
+
+
+# djur = input("Vad är ditt favoritdjur? ")
+
+# print(f"Ditt favoritdjur är {djur}!")
+
+# djur = input("Vad är ditt favoritdjur? ")
+# djur = djur.upper()
+
+# print(f"Ditt favoritdjur är {djur}!")
+
+# min_text = "hejsan!"
+
+# print(len(min_text))
+
+# min_text = ""
+
+# print(len(min_text))
+
+
+# nummer = [5,15,25,35,45]
+# print(sum(nummer))
+
+
+# frukter = ["äpple", "banan", "apelsin", "blåbär"]
+# frukter.remove("äpple")
+# for i in frukter:
+#     print(i)
+
+# def räkna_e(text):
+#     return text.lower().count("e")
+
+# mening = "Hej, trevligt att träffas!"
+# antal_e = räkna_e(mening)
+# print(f"Bokstaven e förekommer {antal_e} gånger.")
+
+
+# min_meny = ["Pizza", "Hamburgare", "Sallad", "Soppa", "Pasta"]
+# print(min_meny[::3])
+# print(min_meny[::-1])
+
+# def mult(x, y=10):
+#     svar = (x*y)
+#     print(svar)
+
+# mult(5, 4)
+
+# mult()
+
+# min_lista = [5,10,15,20,25]
+# print(min_lista[0:1])
+# print(min_lista[-2:-1])
+# print(min_lista[10]) # Error
